4b976b5480cd7653e5982a94a0362ba4f508f027
prad <pradhyun.mahasivabhattu@gmail.com>
2025-01-21 12:37:41 +0000

    --

--- DynamicAIWebsites/settings.py
+++ DynamicAIWebsites/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-29z^6(72b8vdidk!ul_*0eyn8qb#ng9q93h863$d#^e&4nmny+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # Default primary key field type
